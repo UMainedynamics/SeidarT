eb4ceedc109f6dcf549b8018d6db69b75b34c5dd
sbernsen <stevenbernsen@gmail.com>
2024-08-28 16:38:14 -0400

    updates

--- setup.py
+++ setup.py
@@ -1,55 +1,49 @@
-from numpy.distutils.core import setup
-from numpy.distutils.misc_util import Configuration
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import numpy.f2py 
 
-def configuration(parent_package='', top_path=None):
-    config = Configuration(None, parent_package, top_path)
+# class CustomBuildExt(build_ext):
+#     def build_extensions(self):
+#         # Add the Fortran compiler
+#         for ext in self.extensions:
+#             if ext.name == 'seidart.fortran.cpmlfdtd':
+#                 ext.extra_f90_compile_args = ['-std=f95']
+#         super().build_extensions()
 
-    # Define Fortran extensions
-    fortran_sources = [
-        'src/seidart/fortran/cpmlfdtd.f95',
-        # 'src/seidart/fortran/orientsynth.f95'
-    ]
-    
-    config.add_extension(
-        name='seidart.fortran.cpmlfdtd',
-        sources=fortran_sources[0],
-        # extra_f90_compile_args=['m64'],
-    )
-    # config.add_extension(
-    #     name='seidart.fortran.orientsynth',
-    #     sources = fortran_sources[1],
-    #     # extra_f90_compile_args=['m64'],
-    # )
-    
-    return config
-
-if __name__ == "__main__":
-    setup(
-        name='seidart',
-        version='2.2.3',
-        packages=[
-            'seidart', 
-            'seidart.fortran', 
-            'seidart.routines', 
-            'seidart.simulations', 
-            'seidart.visualization'
-        ],
-        configuration=configuration,
-        entry_points = {
-            'console_scripts': [
-                'prjbuild=seidart.routines.prjbuild:main',
-                'prjrun=seidart.routines.prjrun:main',
-                'arraybuild=seidart.routines.arraybuild:main',
-                'sourcefunction=seidart.routines.sourcefunction:main',
-                'rcxdisplay=seidart.visualization.rcxdisplay:main',
-                'im2anim=seidart.visualiztion.im2anim:build_animation',
-            ]
-        },
-        install_requires=[
-            'numpy',
-            'setuptools',
-            'wheel',
-            'matplotlib',
-            'seaborn'
+setup(
+    name='seidart',
+    version='2.2.3',
+    packages=[
+        'seidart',
+        'seidart.fortran',
+        'seidart.routines',
+        'seidart.simulations',
+        'seidart.visualization'
+    ],
+    ext_modules=[
+        Extension(
+            name='seidart.fortran.cpmlfdtd',
+            sources=['src/seidart/fortran/cpmlfdtd.f95'],
+            include_dirs=[numpy.get_include()],
+        ),
+    ],
+    # cmdclass={'build_ext': CustomBuildExt},
+    entry_points={
+        'console_scripts': [
+            'prjbuild=seidart.routines.prjbuild:main',
+            'prjrun=seidart.routines.prjrun:main',
+            'arraybuild=seidart.routines.arraybuild:main',
+            'sourcefunction=seidart.routines.sourcefunction:main',
+            'rcxdisplay=seidart.visualization.rcxdisplay:main',
+            'im2anim=seidart.visualiztion.im2anim:build_animation',
+            # other entry points...
         ]
-    )
+    },
+    install_requires=[
+        'numpy',
+        'setuptools',
+        'wheel',
+        'matplotlib',
+        'seaborn'
+    ]
+)
