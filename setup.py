b727ab307da42f33580d2acf15bdd8172590230f
sbernsen <stevenbernsen@gmail.com>
2024-11-21 11:09:13 -0500

    setup.py updates

--- setup.py
+++ setup.py
@@ -16,17 +16,14 @@ class CustomInstallCommand(install):
             arch = platform.machine().lower()
             
             binary_name = f'seidartfdtd-{system}-{arch}'
-            # if system == 'darwin' and arch == 'arm64':
-            #     binary_name = 'seidartfdtd-darwin-arm64'
-            # elif system == 'darwin' and arch == 'x86_64':
-            #     binary_name = 'seidartfdtd-darwin-x86_64'
-            # elif system == 'linux' and arch == 'x86_64':
-            #     binary_name = 'seidartfdtd-linux-x86_64'
-            # else:
-            if system != 'darwin' or system != 'linux':
-                raise RuntimeError("Unsupported platform {}-{}".format(system,arch))
+            if system == 'darwin' and arch == 'arm64':
+                binary_name = 'seidartfdtd-darwin-arm64'
+            elif system == 'darwin' and arch == 'x86_64':
+                binary_name = 'seidartfdtd-darwin-x86_64'
+            elif system == 'linux' and arch == 'x86_64':
+                binary_name = 'seidartfdtd-linux-x86_64'
             else:
-                print(binary_name)
+                raise RuntimeError("Unsupported platform {}-{}".format(system,arch))
             
             src_binary_path = os.path.join('src', 'seidart', 'binaries', binary_name)
             dest_binary_path = os.path.join(env_bin_path, 'seidartfdtd')
